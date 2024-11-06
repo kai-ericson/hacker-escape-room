cd9f89be9337e3c08ed82c481e083f3a47a892a5
Kai <kai.ericson@lerniastudent.se>
2024-11-06 13:36:57 +0100

    menu button created and eventlistener added

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const menuButton = document.querySelector(".hamburgerButton");
+const app = document.querySelector(".container");
+menuButton.addEventListener("click", showMenu);
+console.log("in JS");
+function showMenu(){
+    console.log("button pressed");
+    
+}
