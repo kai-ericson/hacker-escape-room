195b07ccc612a364aed83a5a91eee42e7acfcd67
Kai <kai.ericson@lerniastudent.se>
2024-11-08 13:20:49 +0100

    add buttons functionality and make close button

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 const menuButton = document.querySelector(".hamburgerButton");
-const app = document.querySelector(".container");
+const app = document.querySelector("body");
+const menu=document.querySelector(".mobileMenu");
+const closeButton=document.querySelector(".closeButton");
 menuButton.addEventListener("click", showMenu);
+closeButton.addEventListener("click",hideMenu);
+
 console.log("in JS");
 function showMenu(){
-    console.log("button pressed");
-    
+    menu.setAttribute("class","mobileMenuActive");
+    menu.style.display="inline";
+}
+
+function hideMenu(){
+    menu.removeAttribute("class","mobileMenuActive");
+    menu.style.display="none";
 }
